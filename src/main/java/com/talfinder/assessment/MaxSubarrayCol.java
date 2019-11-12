d296ef8d1e820cc6e04857dd718a349d49c7892e
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2019-11-12 08:34:43 +0530

    Initial commit

---
+++ src/main/java/com/talfinder/assessment/MaxSubarrayCol.java
@@ -0,0 +1,26 @@
+package com.talfinder.assessment;
+
+public class MaxSubarrayCol {
+
+    public static int maxSubarrayCol(int[][] matrix) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 0,  1,  2},
+                { 2,  4, -1},
+                { 3,  6, -2},
+                {-1, -1,  6},
+        };
+        int rowWithMaxSubArray = maxSubarrayCol(matrix);
+        System.out.println("Row with maximum subarray is:" + rowWithMaxSubArray);
+    }
+}
